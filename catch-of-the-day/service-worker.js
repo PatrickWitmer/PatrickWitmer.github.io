30811238fb3488efff45675537260c593d24ac3b
WitmerDesigns <patrick@witmerdesigns.com>
2019-11-26 16:24:45 -0700

    new build try for cotd

--- catch-of-the-day/service-worker.js
+++ catch-of-the-day/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a626fd8f93e67712c805e1a5232cc800.js"
+  "/catch-of-the-day/precache-manifest.47b7ea83ff2c8dd0cc09036e2857bc06.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/catch-of-the-day/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
