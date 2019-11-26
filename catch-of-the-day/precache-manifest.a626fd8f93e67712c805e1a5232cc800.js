24175f01410e2ea07b6b93485c7e06114180d9e7
WitmerDesigns <patrick@witmerdesigns.com>
2019-11-26 16:16:15 -0700

    New portfolio piece

---
+++ catch-of-the-day/precache-manifest.a626fd8f93e67712c805e1a5232cc800.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ce9283a771d4b6fc165",
+    "url": "/static/css/main.fddeb503.chunk.css"
+  },
+  {
+    "revision": "4ce9283a771d4b6fc165",
+    "url": "/static/js/main.90350f23.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8397eae9d5dc82b2b858",
+    "url": "/static/js/2.48e23ad3.chunk.js"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/static/media/haymaker-webfont.e9535f20.ttf"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "47e26dee9b053b540478750bdf7a87d5",
+    "url": "/static/media/haymaker-webfont.47e26dee.svg"
+  },
+  {
+    "revision": "c175fe49ca6c599199a81c6791490344",
+    "url": "/static/media/blanch_caps_inline-webfont.c175fe49.svg"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "11b62655ea63e1ecb8d1b02848aa0a83",
+    "url": "/index.html"
+  }
+];
